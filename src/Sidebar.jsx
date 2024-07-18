7ed34945b99da3be6ae2828b2b1cb9b97930a2e9
Usman-837 <usmantufail837@gmail.com>
2024-07-18 12:30:58 +0500

    add images in assets folder, remove favicon and change title name in index.html, add custom css in index.css and add logo image and heading in sidebar component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import logo from "./assets/logo.png";
 
 const Sidebar = () => {
     return(
         <>
+        <div className="bg-[#071539] w-[15vw] h-[150vh]">
+            {/* logo */}
+            <div className="flex justify-center gap-2 pt-4">
+                <a href="#"><img src={logo} className="w-[40px] h-[40px]"/></a>
+                <h1 className="text-white text-3xl font-bold pt-1">lector.</h1>
+            </div>
+        </div>
         
         </>
     );
